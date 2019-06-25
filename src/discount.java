934134c34d084e04298d02736eaa3558cf6605e0
Evgeniy <oz.f1est0@gmail.com>
2019-06-25 22:24:12 +0300

    Обновил (комментарии, оптимизация)
    [+] добавил общую библиотеку "java.util.*";
    [+] добавил комментарии к коду;

--- src/discount.java
+++ src/discount.java
@@ -1,26 +1,27 @@
 /* Сумма покупки составляет а рублей. Если а больше 1000 рублей, то предоставляется скидка 15%. Вывести на экран сумму
-покупки с учетом скидки либо сообщение о том, что скидка не предоставляется.
-*/
+покупки с учетом скидки либо сообщение о том, что скидка не предоставляется. */
 
-import java.util.Scanner;
+import java.util.*;
 
-public class Program
-{
-	public static void main(String[] args) {
-		
-		Scanner amountEnter = new Scanner(System.in);
-		int amount = amountEnter.nextInt();
-		double discount = amount * 0.15;
-		int conToInt = (int)discount; // converting to integer
-		int resultDiscount;
-		
-		if (amount >= 1000) {
-			resultDiscount = amount - conToInt;
-			System.out.println("Purchase amount with discount: " + resultDiscount + "rub.");
-		} else {
-			System.out.println("Discount not available.");
-		}
-		
-		amountEnter.close();
-	}
+public class Main {
+
+    public static void main(String[] args) {
+
+        // Просим ввести стоимость покупки.
+        Scanner amountEnter = new Scanner(System.in);
+        System.out.print("Enter amount: ");
+        int amount = amountEnter.nextInt();
+
+        // Считаем скидку в 15%
+        double discount = amount * 0.15;
+        int conToInt = (int) discount;
+        int resultDiscount;
+
+        if (amount >= 1000) {
+            resultDiscount = amount - conToInt;
+            System.out.println("Purchase amount with discount: " + resultDiscount + "rub.");
+        } else {
+            System.out.println("Discount not available.");
+        }
+  }
 }
