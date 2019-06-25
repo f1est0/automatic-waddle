b11e03589953f43fa87983fb330103f4ef834463
Evgeniy <oz.f1est0@gmail.com>
2019-06-25 21:52:07 +0300

    Обновил (цикл, комментарии)
    [+] поменял цикл while, на for;
    [+] добавил парочку комментариев;
    [+] вывод цифр, любого размера массива;

--- src/arr.java
+++ src/arr.java
@@ -3,31 +3,25 @@ import java.util.*;
 public class Main {
 
     public static void main(String[] args) {
-
+        int r = 0;
         Scanner sc = new Scanner(System.in);
 
+        // Указываем размер массива, для этого просим ввести целое число, преобразуем в в int, присваеваем массиву.
         System.out.print("Enter number of digits in the array: ");
         int a = Integer.parseInt(sc.next());
-
-        /*if (a == o) {
-            System.out.print("Aloxa");
-        }*/
-
         int [] arr = new int[a];
-        int ch = 0;
 
-        while (ch != a) {
+        // Вводим цифры в массив, пока незакончится размер.
+        for (int ch = 0; ch != a; ch++) {
             System.out.print("Enter your number: ");
             int i = Integer.parseInt(sc.next());
             arr[ch] = i;
-            ch++;
         }
 
-        System.out.println(arr[0]);
-        System.out.println(arr[1]);
-        System.out.println(arr[2]);
-        System.out.println(arr[3]);
-        System.out.println(arr[4]);
-
+        // Выводим результат с числами и ячейков в массиве.
+        for (int chR = 0; chR != a; chR++){
+            System.out.println("Number from array [" + chR + "]: " + arr[r]);
+            r++;
+        }
     }
 }
